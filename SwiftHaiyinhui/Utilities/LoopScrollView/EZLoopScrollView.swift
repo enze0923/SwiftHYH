af2fe5ee5ffe6fbc4bdf757c829e61a7cc581181
wuez <wuez@chyjr.com>
2016-11-22 16:44:31 +0800

    创建 可点击无限轮滑视图

---
+++ SwiftHaiyinhui/Utilities/LoopScrollView/EZLoopScrollView.swift
@@ -0,0 +1,15 @@
+//
+//  EZLoopScrollView.swift
+//  SwiftHaiyinhui
+//
+//  Created by HaiyinEnze on 2016/11/22.
+//  Copyright © 2016年 ENZE. All rights reserved.
+//
+
+import UIKit
+
+
+class EZLoopScrollView: UIView {
+
+
+}
